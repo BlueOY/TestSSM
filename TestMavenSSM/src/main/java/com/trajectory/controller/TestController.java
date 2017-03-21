65ae2139e9af76e30031dc2c765217090816818e
BlueOY <328082876@qq.com>
2017-03-21 16:01:55 +0800

    testJson

--- TestMavenSSM/src/main/java/com/trajectory/controller/TestController.java
+++ TestMavenSSM/src/main/java/com/trajectory/controller/TestController.java
@@ -1,12 +1,18 @@
 package com.trajectory.controller;
 
+import java.io.PrintWriter;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.trajectory.pojo.Test;
 import com.trajectory.service.ITestService;
 
 @Controller  
@@ -22,6 +28,22 @@ public class TestController {
 		com.trajectory.pojo.Test test = this.testService.getTestById(id);
 		model.addAttribute("test", test);
 		return "showTest";
+	}
+	
+	@RequestMapping("/testJson")
+	public void testJson(HttpServletRequest request, PrintWriter out){
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("key", "value");
+		out.write(jsonObject.toJSONString());
+	}
+	
+	@RequestMapping("/testObject")
+	@ResponseBody
+	public com.trajectory.pojo.Test testObject(HttpServletRequest request){
+		com.trajectory.pojo.Test test = new com.trajectory.pojo.Test();
+		test.setId(1);
+		test.setName("name");
+		return test;
 	}
 
 }
